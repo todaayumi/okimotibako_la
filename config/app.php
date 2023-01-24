fdb74e1748c174344db810a270067de28ef2efef
todaayumi <ayumin.amg25@docomo.ne.jp>
2023-01-24 14:21:57 +0900

    エラー解消

--- config/app.php
+++ config/app.php
@@ -174,8 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \SocialiteProviders\Manager\ServiceProvider::class, 
-
+        Laravel\Socialite\SocialiteServiceProvider::class,
     ],
 
     /*
