16d4c6a236bd62b3f2f98d6ddc96c735c9318f86
todaayumi <ayumin.amg25@docomo.ne.jp>
2023-01-24 13:16:28 +0900

    コミット漏れ

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        SocialiteWasCalled::class => [
+            'SocialiteProviders\\Twitter\\TwitterExtendSocialite@handle',
+        ],
     ];
 
     /**
